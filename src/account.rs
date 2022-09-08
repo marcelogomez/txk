3c9f9920b0b00c629ff9b6a269a77104a9b11f38
Marcelo Gomez <marcelogomez321@gmail.com>
2022-09-08 14:41:28 +0100

    simple tests for deposits and withdrawals

--- src/account.rs
+++ src/account.rs
@@ -27,7 +27,8 @@ impl Account {
     }
 
     pub fn deposit(&mut self, amount: Funds) -> Result<(), AccountUpdateError> {
-        self.balance
+        self.balance = self
+            .balance
             .apply(BalanceDiff::new().with_available(amount))?;
         Ok(())
     }
@@ -37,8 +38,46 @@ impl Account {
             return Err(AccountUpdateError::InsufficientFunds);
         }
 
-        self.balance
+        self.balance = self
+            .balance
             .apply(BalanceDiff::new().with_available(-amount))?;
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::funds::Funds;
+    use rust_decimal_macros::dec;
+
+    #[test]
+    fn test_deposit() {
+        let mut account = Account::new(42);
+        account
+            .deposit(Funds::new(dec!(1.5)))
+            .expect("Deposit to succeed");
+        assert_eq!(account.balance.available(), Funds::new(dec!(1.5)));
+    }
+
+    #[test]
+    fn test_withdrawal() {
+        let mut account = Account::new(42);
+        account
+            .deposit(Funds::new(dec!(1.5)))
+            .expect("Deposit to succeed");
+        account
+            .withdraw(Funds::new(dec!(1.0)))
+            .expect("Withrawal to succeed");
+        assert_eq!(account.balance.available(), Funds::new(dec!(0.5)));
+    }
+
+    #[test]
+    fn test_withdrawal_insufficient_funds() {
+        let mut account = Account::new(42);
+        assert_eq!(
+            account.withdraw(Funds::new(dec!(1.5))),
+            Err(AccountUpdateError::InsufficientFunds),
+        );
     }
 }
