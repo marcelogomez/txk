200d1357417dab61555ddfce5c49866a6a075c9e
Marcelo Gomez <marcelogomez321@gmail.com>
2022-09-08 15:07:27 +0100

    implement disputes

--- src/account.rs
+++ src/account.rs
@@ -3,10 +3,25 @@ use crate::balance::BalanceDiff;
 use crate::funds::Funds;
 use crate::funds::FundsOpError;
 use crate::transaction::ClientID;
+use crate::transaction::TransactionID;
+use std::collections::HashMap;
 use thiserror::Error;
+
+enum DepositState {
+    Undisputed(Funds),
+    InDispute(Funds),
+    Resolved,
+    Chargedback,
+}
 
 #[derive(Debug, PartialEq, Error)]
 pub enum AccountUpdateError {
+    #[error("Transaction {0} is not disputable (has already been settled or not a deposit)")]
+    TransactionNotDisputable(TransactionID),
+    #[error("Transaction {0} is not in dispute")]
+    TransactionNotInDispute(TransactionID),
+    #[error("Deposit {0} already processed")]
+    DepositAlreadyProcessed(TransactionID),
     #[error("Insufficient funds")]
     InsufficientFunds,
     #[error("Failed to update balance: {0}")]
@@ -16,6 +31,7 @@ pub enum AccountUpdateError {
 pub struct Account {
     client: ClientID,
     balance: Balance,
+    deposits: HashMap<TransactionID, DepositState>,
 }
 
 impl Account {
@@ -23,13 +39,25 @@ impl Account {
         Self {
             client,
             balance: Balance::new(),
+            deposits: HashMap::new(),
         }
     }
 
-    pub fn deposit(&mut self, amount: Funds) -> Result<(), AccountUpdateError> {
+    pub fn deposit(
+        &mut self,
+        transaction_id: TransactionID,
+        amount: Funds,
+    ) -> Result<(), AccountUpdateError> {
+        if self.deposits.contains_key(&transaction_id) {
+            return Err(AccountUpdateError::DepositAlreadyProcessed(transaction_id));
+        }
+
         self.balance = self
             .balance
             .apply(BalanceDiff::new().with_available(amount))?;
+        self.deposits
+            .insert(transaction_id, DepositState::Undisputed(amount));
+
         Ok(())
     }
 
@@ -41,7 +69,51 @@ impl Account {
         self.balance = self
             .balance
             .apply(BalanceDiff::new().with_available(-amount))?;
+
         Ok(())
+    }
+
+    pub fn dispute(&mut self, transaction_id: TransactionID) -> Result<(), AccountUpdateError> {
+        if let Some(&DepositState::Undisputed(amount)) = self.deposits.get(&transaction_id) {
+            self.balance = self
+                .balance
+                .apply(BalanceDiff::new().with_available(-amount).with_held(amount))?;
+            self.deposits
+                .insert(transaction_id, DepositState::InDispute(amount));
+
+            Ok(())
+        } else {
+            Err(AccountUpdateError::TransactionNotDisputable(transaction_id))
+        }
+    }
+
+    pub fn resolve(&mut self, transaction_id: TransactionID) -> Result<(), AccountUpdateError> {
+        if let Some(&DepositState::InDispute(amount)) = self.deposits.get(&transaction_id) {
+            self.balance = self
+                .balance
+                .apply(BalanceDiff::new().with_available(amount).with_held(-amount))?;
+            self.deposits.insert(transaction_id, DepositState::Resolved);
+
+            Ok(())
+        } else {
+            Err(AccountUpdateError::TransactionNotInDispute(transaction_id))
+        }
+    }
+
+    pub fn chargeback(&mut self, transaction_id: TransactionID) -> Result<(), AccountUpdateError> {
+        if let Some(&DepositState::InDispute(amount)) = self.deposits.get(&transaction_id) {
+            self.balance = self.balance.apply(
+                BalanceDiff::new()
+                    .with_available(-amount)
+                    .with_held(-amount),
+            )?;
+            self.deposits
+                .insert(transaction_id, DepositState::Chargedback);
+
+            Ok(())
+        } else {
+            Err(AccountUpdateError::TransactionNotInDispute(transaction_id))
+        }
     }
 }
 
@@ -55,7 +127,7 @@ mod test {
     fn test_deposit() {
         let mut account = Account::new(42);
         account
-            .deposit(Funds::new(dec!(1.5)))
+            .deposit(1, Funds::new(dec!(1.5)))
             .expect("Deposit to succeed");
         assert_eq!(account.balance.available(), Funds::new(dec!(1.5)));
     }
@@ -64,7 +136,7 @@ mod test {
     fn test_withdrawal() {
         let mut account = Account::new(42);
         account
-            .deposit(Funds::new(dec!(1.5)))
+            .deposit(1, Funds::new(dec!(1.5)))
             .expect("Deposit to succeed");
         account
             .withdraw(Funds::new(dec!(1.0)))
