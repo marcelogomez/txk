c94186a6a9d984742e61ec643855423dd08cbc80
Marcelo Gomez <marcelogomez321@gmail.com>
2022-09-08 13:37:10 +0100

    deserailisation for transactions

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,28 +1,72 @@
+use serde::Deserialize;
+
 pub type ClientID = u16;
 pub type TransactionID = u32;
 
-#[derive(Debug, PartialEq)]
-pub enum Transaction {
-    Deposit {
-        client: ClientID,
-        transaction: TransactionID,
-        amount: f32,
-    },
-    Withdrawal {
-        client: ClientID,
-        transaction: TransactionID,
-        amount: f32,
-    },
-    Dispute {
-        client: ClientID,
-        transaction: TransactionID,
-    },
-    Resolve {
-        client: ClientID,
-        transaction: TransactionID,
-    },
-    Chargeback {
-        client: ClientID,
-        transaction: TransactionID,
-    },
+#[derive(Debug, PartialEq, Eq, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum TransactionType {
+    Deposit,
+    Withdrawal,
+    Dispute,
+    Resolve,
+    Chargeback,
+}
+
+// Ideally this would be an enum to account for the fact that
+// amount is only presennt for deposits and withdrawals.
+// However, the csv crate does not deal very well with tagged enum
+// deserialization (see https://github.com/BurntSushi/rust-csv/issues/278).
+// Instead we opt to make amount an Option. This has some implications for serialisation:
+// because all records need to have the same amount of columns we need a trailing comma for 
+// records that do not have an amount 
+#[derive(Debug, PartialEq, Deserialize)]
+pub struct Transaction {
+    #[serde(rename = "type")]
+    tx_type: TransactionType,
+    client: ClientID,
+    #[serde(rename = "tx")]
+    transaction: TransactionID,
+    amount: Option<f32>,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use csv::Reader;
+
+    fn deserialize_transaction_from_str(t: &str) -> Transaction {
+        Reader::from_reader(format!("type,client,tx,amount\n{}", t).as_bytes())
+            .deserialize::<Transaction>()
+            .next()
+            .expect("One element")
+            .expect("Serialization to succeed")
+    }
+
+    #[test]
+    fn test_deserialize_deposit() {
+        assert_eq!(
+            deserialize_transaction_from_str("deposit,1,1,1.0"),
+            Transaction {
+                tx_type: TransactionType::Deposit,
+                client: 1,
+                transaction: 1,
+                amount: Some(1.0),
+            },
+        );
+    }
+
+    #[test]
+    fn test_deserialize_dispute() {
+        assert_eq!(
+            // Note that we need a trailing comma. This is because amount is an Option
+            deserialize_transaction_from_str("dispute,1,1,"),
+            Transaction {
+                tx_type: TransactionType::Dispute,
+                client: 1,
+                transaction: 1,
+                amount: None,
+            },
+        );
+    }
 }
